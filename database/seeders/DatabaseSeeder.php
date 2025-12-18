622389673923e330b5f1b74974a126fc29388c48
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2025-12-18 20:18:56 +0100

    initialize

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::firstOrCreate(
+            ['email' => 'admin@email.com'],
+            [
+                'name' => 'Admin User',
+                'password' => bcrypt('123456'),
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
